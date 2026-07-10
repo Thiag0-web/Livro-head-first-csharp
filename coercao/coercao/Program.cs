80ae2d4bbd0d97dda3bd58c0d9e42cdb68e16372
Thiag0-web <thiago.fullstacker@gmail.com>
2026-07-10 11:24:48 -0300

    Estudando coerção e fazendo exercicios.

---
+++ coercao/coercao/Program.cs
@@ -0,0 +1,16 @@
+﻿int myInt = 10;
+byte myByte = (byte)myInt;
+double myDouble = (double)myByte;
+bool myBool = (bool)myDouble;
+string myString = "false";
+myBool = (bool)myString;
+myString = (string)myInt;
+myString = myInt.ToString();
+myByte = (bool)myByte;
+myBool = (bool)myBool;
+short myShort = (short)myInt;
+char myChar = 'x';
+myString = (string)myChar;
+long myLong = (long)myInt;
+decimal myDecimal = (decimal)myLong;
+myString = myString + myInt + myByte + myDouble + myChar;
---
+++ temporario/temporario/Program.cs
@@ -0,0 +1,5 @@
+﻿int myInt = 36;
+float myFloat = 16.4F;
+myFloat = myInt + myFloat;
+Console.WriteLine(myFloat);
+Console.WriteLine(myInt);
