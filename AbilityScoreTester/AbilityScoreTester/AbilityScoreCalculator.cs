c647a8d8ffe6ecf54455b32629494683f3466bb4
Thiag0-web <thiago.fullstacker@gmail.com>
2026-07-13 10:54:12 -0300

    aprendendo mais sobre tipos e referências

---
+++ AbilityScoreTester/AbilityScoreTester/AbilityScoreCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbilityScore
+{
+    public class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            // Divide the roll result by the DivideBy field
+            double divided = RollResult / DivideBy;
+
+            // Add AddAmount to the result of that division
+            int added = AddAmount + (int)divided;
+
+            // If the result is too small, use Minimum
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+            if (0.1 + 0.2 == 0.3) Console.WriteLine("They're equal");
+            else Console.WriteLine("They aren't equal");
+        }
+    }
+}
---
+++ AbilityScoreTester/AbilityScoreTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AbilityScoreTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AbilityScoreTester")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("331b7a1f-d817-4bd4-8eab-70941d4215ae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
